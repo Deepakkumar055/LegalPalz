192f7de19110e55940076bac406eaca22351509d
Deepakkumar055 <deepakkumr2098@gmail.com>
2024-06-22 20:14:42 +0530

    made changes

--- include/contact-process.php
+++ include/contact-process.php
@@ -31,3 +31,19 @@ if (isset($_POST['email'])) {
 	die('Invalid entry!');
 }
 ?>
+
+
+<?php
+// db_connect.php
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
